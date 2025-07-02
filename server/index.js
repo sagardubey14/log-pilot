9894210ee7be83d08318541d067f969cfc5a8f6c
sagardubey14 <sagardubey353@gmail.com>
2025-07-02 17:44:44 +0530

    initial backend setup with project structure

--- server/index.js
+++ server/index.js
@@ -1,1 +1,9 @@
-console.log('log-pilot-backend.');
+
+const app = require('./src/app');
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+    console.log('Welcom to Log Pilot/n');
+    
+  console.log(`Server running at http://localhost:${PORT}`);
+});
